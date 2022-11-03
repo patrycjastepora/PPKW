b51ef2958ade3d2dfcd4108880a48ae3aae86d44
patrycjastepora <85075600+patrycjastepora@users.noreply.github.com>
2022-11-03 13:00:14 +0100

    add function check str

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -2,12 +2,39 @@
 import http.server
 import socketserver
 import os
-import datetime
 
 #print('source code for "http.server":', http.server.__file__)
 
 class web_server(http.server.SimpleHTTPRequestHandler):
+    
+    def do_GET(self):
+
+        print(self.path)
+        
+        if self.path == '/':
+            self.protocol_version = 'HTTP/1.1'
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=UTF-8")
+            self.end_headers()            
+            self.wfile.write(b"Hello World!\n")
+        else:
+            super().do_GET()
             
+    def check_string(userStr): 
+    	special_characters=""!@#$%^&*()-+?_+<>,/""
+    	d={"lowercase":0, "uppercase":0, "digits":0, "special":0}
+    	for c in userStr:
+    	    if c.islower():
+    	       d["lowercase"]+=1
+    	    elif c.isupper():
+    	       d["uppercase"]+=1
+    	    elif c.isdigit():
+    	       d["digits"]+=1
+    	    elif any(c in special_characters for c in userStr):
+    	       d["special"]+=1
+    	    else: 
+    	    	
+    
 # --- main ---
 
 PORT = 4080

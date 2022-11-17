613e48a62d3808f4a70ee7f73e7dabec52044ab1
patrycjastepora <85075600+patrycjastepora@users.noreply.github.com>
2022-11-17 13:07:20 +0100

    fix problems

--- lab4/source/server.py
+++ lab4/source/server.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, request
 import re
-import json
 
 # print('source code for "http.server":', http.server.__file__)
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=['GET'])
+def input_numbers():
+	return math_operations(request.args.get('num1'), request.args.get('num2'))
+
 def math_operations(num1, num2):
 	num1 = int(num1)
 	num2 = int(num2)
 	d = { "sum" : num1+num2, "sub" : num1-num2, "mul" : num1*num2, "div" : num1/num2, "mod" : num1%num2}
-	return json.dump(d)
-
+	return d
+	
 # --- main ---
 if __name__ = '__main__':
 
@@ -21,3 +23,4 @@ if __name__ = '__main__':
 	print(f'Starting: http://localhost:{PORT}')
 	
 	app.run(debug=False,host='0.0.0.0',port=PORT)
+	

259a04e56d107f0ab55f2fec82ca838240ac1957
patrycjastepora <85075600+patrycjastepora@users.noreply.github.com>
2022-12-08 12:33:30 +0100

    operations function

--- lab5/source/server.py
+++ lab5/source/server.py
@@ -1,30 +1,10 @@
 #!/usr/bin/env python3
-import http.server
-import socketserver
-import os
+from flask import Flask, request, jsonify
 
-#print('source code for "http.server":', http.server.__file__)
+app=Flask(__name__)
 
-class web_server(http.server.SimpleHTTPRequestHandler):
-    
-    def do_GET(self):
-
-        print(self.path)
-        
-        if self.path == '/':
-            self.protocol_version = 'HTTP/1.1'
-            self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
-        else:
-            super().do_GET()
-    
-# --- main ---
-
-PORT = 4080
-
-print(f'Starting: http://localhost:{PORT}')
-
-tcp_server = socketserver.TCPServer(("",PORT), web_server)
-tcp_server.serve_forever()
+def operation(num1, num2):
+	return [num1+num2,num1-num2,num1*num2,num1//num2,num1%num2]
+	
+	
+app.run(port=4080, host='0.0.0.0')
